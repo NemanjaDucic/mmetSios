a2ad7d116dc3d1c9808a6e97b1ea5c132d9c23d9
NemanjaDucic <ducicnemanja@gmail.com>
2024-04-23 08:32:48 +0200

    newcommit

---
+++ MeetSerbia/Helpers/AppUtility.swift
@@ -0,0 +1,8 @@
+//
+//  AppUtility.swift
+//  MeetSerbia
+//
+//  Created by Nemanja Ducic on 17.4.24..
+//
+
+import Foundation
---
+++ MeetSerbia/VCTEST.swift
@@ -0,0 +1,8 @@
+//
+//  VCTEST.swift
+//  MeetSerbia
+//
+//  Created by Nemanja Ducic on 12.4.24..
+//
+
+import Foundation
